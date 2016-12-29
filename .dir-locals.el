702bb6b06332929709e649598df932aa282c244b
Tomas Abrahamsson <tomas.abrahamsson@gmail.com>
2016-12-29 11:18:27 +0100

    Add a project-wide .dir-locals.el for Emacs

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;; Project-wide Emacs settings
+(
+ ;; `nil' settings apply to all language modes
+ (nil
+  ;; Use only spaces for indentation
+  (indent-tabs-mode . nil))
+ (erlang-mode
+  ;;
+  (erlang-indent-level . 4)))
