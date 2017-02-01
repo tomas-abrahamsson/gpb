ec0a60d27a32fea3539d3332c204999cfe058e69
Tomas Abrahamsson <tomas.abrahamsson@gmail.com>
2017-02-01 18:03:42 +0100

    Tell .dir-locals.el to indent Makefiles with tabs
    While at it, also specify how wide a tab is, and set fill-column

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,9 +1,18 @@
 ;; Project-wide Emacs settings
 (
- ;; `nil' settings apply to all language modes
- (nil
-  ;; Use only spaces for indentation
-  (indent-tabs-mode . nil))
- (erlang-mode
-  ;;
-  (erlang-indent-level . 4)))
+ ;; For all modes (types of files)
+ (nil . (
+	 ;; use spaces to indent, not tabs
+	 (indent-tabs-mode . nil)
+	 ;; but if there are any tabs, the tab with is 8 chars anyway
+	 (tab-width . 8)
+	 ;; Lines no longer than this
+	 (fill-column . 80)))
+
+ ;; However, for Makefiles, tabs are important
+ (makefile-mode . ((indent-tabs-mode . t)))
+
+ ;; For Erlang files
+ (erlang-mode . (
+		 ;; Use this spaces of indentation per level
+		 (erlang-indent-level . 4))))
