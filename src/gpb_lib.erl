3774a0a6725d5d8abb6d6d996dfd38e4893efbfb
Tomas Abrahamsson <tomas.abrahamsson@gmail.com>
2017-06-16 19:59:34 +0200

    Break out type formatting to its own file

--- src/gpb_lib.erl
+++ src/gpb_lib.erl
@@ -22,7 +22,9 @@
 
 -module(gpb_lib).
 
--export([mk_fn/2]).
+-include("gpb_compile.hrl").
+
+-export([mk_fn/2, mk_fn/3]).
 -export([replace_term/2]).
 -export([replace_tree/2]).
 -export([splice_trees/2]).
@@ -37,18 +39,26 @@
 -export([get_records_or_maps_by_opts/1]).
 -export([get_mapping_and_unset_by_opts/1]).
 -export([get_strings_as_binaries_by_opts/1]).
+-export([get_type_specs_by_opts/1]).
+-export([is_target_major_version_at_least/2]).
 
 -export([index_seq/1]).
 -export([indent/2, indent_lines/2]).
 -export([outdent_first/1]).
 -export([split_indent_iolist/2]).
 -export([linesplit_iolist/1]).
+-export([iolist_to_utf8_or_escaped_binary/2]).
 
 -include("../include/gpb.hrl").
 
 
 mk_fn(Prefix, Suffix) ->
     list_to_atom(lists:concat([Prefix, Suffix])).
+
+mk_fn(Prefix, Middlefix, Suffix) when is_integer(Middlefix) ->
+    mk_fn(Prefix, list_to_atom(integer_to_list(Middlefix)), Suffix);
+mk_fn(Prefix, Middlefix, Suffix) ->
+    list_to_atom(lists:concat([Prefix, Middlefix, "_", Suffix])).
 
 %% Helpers for gpb_codegen parse tree transform operations -----------
 replace_term(Marker, NewTerm) when is_atom(Marker) ->
@@ -106,6 +116,42 @@ get_mapping_and_unset_by_opts(Opts) ->
 get_strings_as_binaries_by_opts(Opts) ->
     proplists:get_bool(strings_as_binaries, Opts).
 
+get_type_specs_by_opts(Opts) ->
+    Default = false,
+    proplists:get_value(type_specs, Opts, Default).
+
+is_target_major_version_at_least(VsnMin, Opts) ->
+    case proplists:get_value(target_erlang_version, Opts, current) of
+        current ->
+            is_current_major_version_at_least(VsnMin);
+        N when is_integer(N) ->
+            N >= VsnMin
+    end.
+
+is_current_major_version_at_least(VsnMin) ->
+    case erlang:system_info(otp_release) of
+        "R"++Rest -> % R16 or ealier
+            FirstChunkOfDigits = lists:takewhile(fun is_digit/1, Rest),
+            list_to_integer(FirstChunkOfDigits) >= VsnMin;
+        RelStr ->
+            %% In Erlang 17 the leading "R" was dropped
+            %% The exact format isn't super documented,
+            %% so be prepared for some (future?) alternatives.
+            try list_to_integer(RelStr) of
+                N when is_integer(N) -> N >= VsnMin
+            catch error:badarg ->
+                    [NStr | _] = string:tokens(RelStr, ".-"),
+                    try list_to_integer(NStr) of
+                        N when is_integer(N) -> N >= VsnMin
+                    catch error:badarg ->
+                            false
+                    end
+            end
+    end.
+
+is_digit(C) when $0 =< C, C =< $9 -> true;
+is_digit(_) -> false.
+
 %% Misc ---
 
 index_seq([]) -> [];
@@ -130,3 +176,35 @@ split_indent_iolist(Indent, IoList) ->
 linesplit_iolist(Iolist) ->
     re:split(Iolist, ["\n"], [trim, {return,binary}]).
 
+iolist_to_utf8_or_escaped_binary(IoList, Opts) ->
+    case understands_coding(Opts) of
+        true  ->
+            unicode:characters_to_binary(
+              ["%% -*- coding: utf-8 -*-\n",
+               IoList]);
+        false ->
+            %% What to do if on Erlang R15 or earlier?  We can't utf8-encode
+            %% the file, because Erlang R15 will read it as latin1.
+            %%
+            %% For now, Assume such encodings are in strings only.
+            %% So far, this is safe, since neither message names nor field
+            %% names nor enum symbols are allowed to be non-ascii.
+            %%
+            %% This means only place for non-ascii is in comments and
+            %% in default strings. Hope I haven't overlooked some
+            %% important place...
+            iolist_to_binary(esc_non_ascii(IoList))
+    end.
+
+understands_coding(Opts) ->
+    %% version   coding: X             default source encoding
+    %% R15:      ignores               latin1
+    %% R16:      understands           latin1
+    %% 17:       understands           utf-8
+    is_target_major_version_at_least(16, Opts).
+
+esc_non_ascii([H|T]) -> [esc_non_ascii(H) | esc_non_ascii(T)];
+esc_non_ascii([])    -> [];
+esc_non_ascii(B) when is_binary(B) -> B;
+esc_non_ascii(C) when is_integer(C), C =< 127 -> C;
+esc_non_ascii(C) when is_integer(C), C > 127  -> ?f("\\x{~.16b}", [C]).
