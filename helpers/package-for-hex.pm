cbcdcca3592f6c2ee629e70a861d42f64aba667c
Tomas Abrahamsson <tomas.abrahamsson@gmail.com>
2021-08-28 17:47:56 +0200

    Switch helper to use rebar3 hex publish
    Previously, it was published using mix

--- helpers/package-for-hex.pm
+++ helpers/package-for-hex.pm
@@ -2,39 +2,29 @@
 
 show_usage() {
     cat <<EOF
-`basename "$0"` -- package gpb for hex.pm and upload it
+`basename "$0"` -- build and upload documentation to hexdoc
 Usage: `basename "$0"` [options]
 Options:
-    -h    Show this help
-    -n    Dry-run: upload nothing: answer 'no' to the question
-          on whether or not to proceed. hex.publish will show you
-          the files it is about to upload.
-          This will also disable the check for whether the
-          version is not on n.m or n.m.x format, ie if we are
-          on a non-tagged git commit.
-    -m    Almost dry-run (moist) This will disable the
-          version format check, but it will not answer 'no' to the
-          proceed question. It will, however, verify that the MIX_HOME
-          and HEX_HOME environment variables are set. These are expected
-          to be set to some mock installation.
+    -h      Show this help
+    -d      Print debug info (set the shell -xv options)
+    -n      Dry-run do not send anything
+    -m      Almost dry-run (moist) This will disable the
+            version format check, but will not send anything
 EOF
 }
 
 dry_run=false
 moist_run=false
-while getopts "hnm" opt
+while getopts "hdnm" opt
 do
     case "$opt" in
-	h) show_usage; exit 0;;
-	n) dry_run=true;;
-	m) moist_run=true;;
+        h) show_usage; exit 0;;
+        d) set -xv;;
+        n) dry_run=true;;
+        m) moist_run=true;;
     esac
 done
-shift $((OPTIND - 1))
-
-# we must execute from inside the gpb repo, or else the
-# version retrieval will fail.
-#
+shift $(($OPTIND - 1))
 
 script_dir="$(dirname "$0")"
 script_path="$(cd "$script_dir"; pwd)"
@@ -42,6 +32,7 @@ cd "$script_dir"
 repo_top=$(git rev-parse --show-toplevel)
 cd "$repo_top"
 repo_path="$(pwd)"
+repo_sha=$(git log -1 --format=%H)
 
 d="$(mktemp -d "tmp-pkg-for-hex-XXXXXXX")"
 cleanup () { /bin/rm -rf "$repo_path/$d"; }
@@ -52,75 +43,88 @@ if [ $dry_run = false -a $moist_run = false ]
 then
     if ! (echo "$vsn" | egrep '^[0-9]+(\.[0-9]+)*$' >/dev/null)
     then
-	echo "ERROR: bad version \"$vsn\", will only publish versions" >&2
-	echo "that are dotted numbers only." >&2
-	echo "Aborting." >&2
-	cleanup
-	exit 1
+        echo "ERROR: bad version \"$vsn\", will only publish versions" >&2
+        echo "that are dotted numbers only." >&2
+        echo "Aborting." >&2
+        cleanup
+        exit 1
     fi
 fi
-
+echo "Version of gpb: $vsn"
 
 set -e
-./helpers/mk-versioned-archive --dest-dir="$d"
 cd "$d"
-tar xfpz gpb-"$vsn".tar.gz
-cd gpb-"$vsn"
 
-cat >package.exs <<EOF
-# -*- coding: utf-8 -*-
-defmodule Gpb.Mixfile do
-  use Mix.Project
+yes_no () {
+    echo "Run?" "$@"
+    valid_answer=false
+    while [ $valid_answer = false ]
+    do
+        printf "Proceed? [Yn]"
+        read x
+        case "$x" in
+            y|Y|yes|Yes|YES|"")
+                valid_answer=true
+                "$@"
+                ;;
+            n|N|no|No|NO)
+                valid_answer=true
+                echo "Not running that command"
+                ;;
+            *)
+                :
+                ;;
+        esac
+    done
+}
 
-  def project do
-    [app: :gpb,
-     version: "$vsn",
-     description: description(),
-     package: package(),
-     deps: deps()]
-  end
+(
 
-  defp deps do
-    []
-  end
-
-  defp description do
-    "A compiler for Google protocol buffer definitions files for Erlang."
-  end
-
-  defp package do
-    [files: ~w(src include descr_src priv bin build gpb.vsn COPYING.LIB README.* rebar.config.script ChangeLog Makefile),
-     maintainers: ["Tomas Abrahamsson"],
-     licenses: ["LGPL 2.1"],
-     links: %{"GitHub" => "https://github.com/tomas-abrahamsson/gpb"},
-     build_tools: ["rebar3", "make"]]
-   end
-end
-
+    git clone -q "$repo_path"
+    cd "$(basename "$repo_path")"
+    unset ERL_LIBS
+    git checkout -q "$repo_sha"
+    make -j all
+    echo "$vsn" > gpb.vsn
+    cat > .publish-tags <<EOF
+[{description,
+  "A compiler for Google protocol buffer definitions files for Erlang."},
+ {files, ["src", "include", "descr_src", "priv", "bin", "build",
+          "gpb.vsn",
+          "COPYING.LIB", "README.*", "rebar.config.script",
+          "ChangeLog", "Makefile"]},
+ {build_tools, [rebar3, make]},
+ {licenses, ["LGPL 2.1"]},
+ {links, [{"GitHub", "https://github.com/tomas-abrahamsson/gpb"}]},
+ {exclude_files, ["include/gpb_version.hrl",
+                  "descr_src/gpb_descriptor.erl",
+                  "descr_src/gpb_descriptor.hrl"]}
+].
 EOF
-
-if [ $dry_run = true ]
-then
-    cat -n package.exs
-    echo
-fi
-if [ $dry_run = true ]
-then
-    echo no | (LC_ALL=en_US.utf-8 MIX_EXS=package.exs mix hex.publish)
-    echo "(answered no)"
-else
-    if [ $moist_run = true ]
+    # Substitute the values above in the gpb.app.src,
+    # retaining file file head.
+    erl +B -noinput -boot no_dot_erlang -eval '
+       AppSrc = "src/gpb.app.src",
+       {ok,B} = file:read_file(AppSrc),
+       {ok,[{application,gpb,Infos}]} = file:consult(AppSrc),
+       {ok,[New]} = file:consult(".publish-tags"),
+       Infos2 = lists:foldl(
+                  fun({K,_}=Item, Acc) -> lists:keystore(K,1,Acc,Item) end,
+                  Infos, New),
+       [FHead | _] = re:split(B, <<"{application,.*gpb">>),
+       B2 = [FHead, io_lib:format("~p.~n", [{application,gpb,Infos2}])],
+       case file:write_file(AppSrc, B2) of
+           ok -> halt(0);
+           Error -> io:format(standard_error, "Error writing ~p:~n  ~p~n",
+                              [AppSrc, Error]),
+                    halt(1)
+       end.
+'
+    if [ $? != 0 ]
     then
-	if [ -z "$MIX_HOME" -o -z "$HEX_HOME" ]
-	then
-	    echo "Extected MIX_HOME and HEX_HOME environment variables" >&2
-	    echo "to be set, presumably to some mocked setup." >&2
-	    echo "  MIX_HOME=\"$MIX_HOME\"" >&2
-	    echo "  HEX_HOME=\"$HEX_HOME\"" >&2
-	    exit 1
-	fi
+        exit 1
     fi
-    (LC_ALL=en_US.utf-8 MIX_EXS=package.exs mix hex.publish package)
-fi
-
-/bin/rm -rf
+    cat src/gpb.app.src
+    export DIAGNOSTIC=1
+    yes_no rebar3 hex publish
+)
